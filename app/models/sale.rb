c9ffcbe325381d84777c5f9f4652bb1d86a6d136
Joshua Rully <joshrully@gmail.com>
2021-03-31 05:27:14 +0000

    revise sale model and add sale partial

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,13 @@
 class Sale < ActiveRecord::Base
-  
+  def expired?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !expired?
+  end
 end
