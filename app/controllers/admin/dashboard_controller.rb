6c90e2eeec683c25db1f3bdb133439a40fc25b42
Joshua Rully <joshrully@gmail.com>
2021-03-31 02:39:56 +0000

    add bcrypt login/signup links to navbar and add view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_NAME'], 
                                password: ENV['ADMIN_PASSWORD']
 
+  before_filter :authorize
+
   def show
     @product_count = Product.count
     @category_count = Product.group(:category_id).count.count
