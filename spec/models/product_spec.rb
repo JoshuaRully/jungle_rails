b645b8bba35091be1325f95205dff622ee385147
Joshua Rully <joshrully@gmail.com>
2021-04-05 07:23:49 +0000

    test that product requires name and price

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,18 +2,45 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    
-    it 'is valid' do
+
+    before do
       @product = Product.new
-      @category = Category.new
-      @category.name = 'test cat'
-      @product.name = 'test prod'
-      @product.price_cents = 1000
-      @product.quantity = 10
-      @product.category = @category
-      expect(@product.valid?).to be true
+    end
+    
+    context 'product created' do
+      it 'is valid' do
+        @category = Category.new
+        @category.name = 'test cat'
+        @product.name = 'test prod'
+        @product.price_cents = 1000
+        @product.quantity = 10
+        @product.category = @category
+        expect(@product.valid?).to be true
+      end
     end
 
-    
+    context 'requires name' do
+      it "name present" do
+        @product.name = nil
+        @product.valid?
+        expect(@product.errors[:name]).to  include("can't be blank")
+
+        @product.name = 'test'
+        @product.valid? 
+        expect(@product.errors[:name]).not_to  include("can't be blank")
+      end
+    end
+
+    context 'requires price' do
+      it "price_cents presence" do
+        @product.price_cents = nil
+        @product.valid?
+        expect(@product.errors[:price_cents]).to  include('is not a number')
+  
+        @product.price_cents = 1000
+        @product.valid?
+        expect(@product.errors[:price_cents]).not_to  include("can't be blank")
+      end
+    end
   end
 end
