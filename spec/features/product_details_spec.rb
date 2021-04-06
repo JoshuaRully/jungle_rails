b1f21dd6241dab021fe54b45d526d61fa7eb5886
Joshua Rully <joshrully@gmail.com>
2021-04-06 06:50:53 +0000

    add more scenarios for thoroughness

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -3,20 +3,32 @@ require 'rails_helper'
 
 RSpec.feature "Visitor clicks product partial and brought to product page", type: :feature, js: true do
 
-    # SETUP
-    before :each do
-      @category = Category.create! name: 'Apparel'
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
 
-      @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
-        description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
-        quantity: 10,
-        price: 64.99
-      )
-    end
+  scenario "They see product name" do
+    # ACT
+    visit root_path
 
-  scenario "They see all products" do
+    # VERIFY
+    find('a.btn-default').click
+
+    expect(page).to have_content('Name')
+    
+    # DEBUG
+    # save_screenshot
+  end
+
+  scenario "They see product description" do
     # ACT
     visit root_path
 
@@ -26,8 +38,33 @@ RSpec.feature "Visitor clicks product partial and brought to product page", type
     expect(page).to have_content('Description')
     
     # DEBUG
-    save_screenshot
+    # save_screenshot
+  end
 
+  scenario "They see product quantity" do
+    # ACT
+    visit root_path
+
+    # VERIFY
+    find('a.btn-default').click
+
+    expect(page).to have_content('Quantity')
+    
+    # DEBUG
+    # save_screenshot
+  end
+
+  scenario "They see product price" do
+    # ACT
+    visit root_path
+
+    # VERIFY
+    find('a.btn-default').click
+
+    expect(page).to have_content('Price')
+    
+    # DEBUG
+    # save_screenshot
   end
 
 end
